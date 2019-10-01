40a77c9fabffc40c18ecfd1e7c0363dd8511736f
Alexey Tsitskun <alexey.tsitskun@yandex.ru>
2019-10-01 12:36:45 +0300

    Basic release from Sprint 12

---
+++ modules/jsonreader.js
@@ -0,0 +1,13 @@
+const fs = require('fs').promises
+
+const jsonreader = (filePath, callback) => {
+  fs.readFile(filePath, { encoding: 'utf8' })
+    .then((data) => {
+      callback(JSON.parse(data))
+    })
+    .catch((err) => {
+      console.log(err)
+      callback(undefined)
+    })
+}
+module.exports = jsonreader
