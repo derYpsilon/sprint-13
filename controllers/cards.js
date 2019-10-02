7b37eb066a59068e0423fde455f336fb181f1d80
Alexey Tsitskun <alexey.tsitskun@yandex.ru>
2019-10-02 20:14:59 +0300

    All the tasks done

--- controllers/cards.js
+++ controllers/cards.js
@@ -21,3 +21,23 @@ module.exports.deleteCard = (req, res) => {
     .then((card) => res.send(card !== null ? { data: card } : { data: 'Nothing to delete' }))
     .catch(() => res.status(500).send({ message: 'Server Controller Error while deleting Card' }))
 }
+
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((card) => res.send({ data: card }))
+    .catch(() => res.status(500).send({ message: 'Server Controller Error while liking Card' }))
+}
+
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((card) => res.send({ data: card }))
+    .catch(() => res.status(500).send({ message: 'Server Controller Error while disliking Card' }))
+}
